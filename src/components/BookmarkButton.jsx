43fef2fa5deb7e33551ee86f2f11dcb0bc2206a6
JacksGo <jackson.golletz@gmail.com>
2021-03-14 19:18:20 -0700

    Emulate Genius page styling
    It's up... and it's good! :football:

---
+++ src/components/BookmarkButton.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { addBookmark, removeBookmark } from '../redux/actions'
+import { getEntityIsBookmarkedById } from '../redux/selectors'
+
+import BookmarkClear from '../assets/bookmark_clear.svg'
+import BookmarkOpaque from '../assets/bookmark_opaque.svg'
+
+export default function BookmarkButton({ plantId }) {
+  const dispatch = useDispatch()
+
+  const bookmarked = useSelector(getEntityIsBookmarkedById(plantId))
+
+  const handleAddBookmark = (e) => {
+    e.preventDefault()
+    dispatch(addBookmark(plantId))
+  }
+
+  const handleRemoveBookmark = (e) => {
+    e.preventDefault()
+    dispatch(removeBookmark(plantId))
+  }
+
+  return (
+    <>
+      { !bookmarked
+        ? <img className="icon" src={BookmarkClear} alt="bookmark" onClick={handleAddBookmark}/>
+        : <img className="icon" src={BookmarkOpaque} alt="unbookmark" onClick={handleRemoveBookmark}/>
+      }
+    </>
+  )
+}
---
+++ src/components/SpeciesCard.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './SpeciesCard.css'
+
+import BookmarkButton from '../components/BookmarkButton'
+import Placeholder from '../assets/generic_plant.svg'
+
+function SpeciesCard(props) {
+  const common = props.name ? props.name : props.scientific
+  const scientific = props.name ? props.scientific : ''
+
+  return (
+    <Link to={{ pathname: `/plant/${props.plantId} ` }}>
+      <div className="species-card">
+        <picture className="species-image">
+          <source srcSet={props.image}/>
+          <img className="species-image-placeholder" src={Placeholder}/>
+        </picture>
+        <div className="species-overlay">
+          <div className="species-name">
+            <div className="species-common">{common}</div>
+            <div className="species-scientific">{scientific}</div>
+          </div>
+          <div className="species-bookmark">
+            <BookmarkButton plantId={props.plantId}/>
+          </div>
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default SpeciesCard
